ac2aee18884a6d79b1249dc77fc7f8ca1ab5a893
Rui <rui.albuquerque.dorey@gmail.com>
2018-02-19 00:07:32 +0000

    adds new components

---
+++ puzzle-bobble/src/logic/prefabs/DynamicCameraPrefab.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Camera } from '../../render/Camera';
+import { isTraveler } from "../components/Traveler";
+
+export class DynamicCameraPrefab extends Camera {
+
+    componentDidMount() {
+        super.componentDidMount();
+        Object.assign( this, isTraveler( this.mesh ) );
+    }
+
+    render() {
+        return <div>DynamicCameraPrefab</div>;
+    }
+}
