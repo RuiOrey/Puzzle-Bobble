b2c563f4dd4718b41502e27098c404d149f2bfe3
Rui <rui.albuquerque.dorey@gmail.com>
2018-02-19 04:03:19 +0000

    changed ui text

--- puzzle-bobble/src/App.js
+++ puzzle-bobble/src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <h1 className="App-title">Welcome to React</h1>
+                    <h1 className="App-title">Puzzle Bobble 3D WebGL</h1>
+
+                    <p >by Rui d'Orey</p>
                 </header>
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
