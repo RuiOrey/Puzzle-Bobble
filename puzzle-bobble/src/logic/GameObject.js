d1b72d2f3021d5353c9fdcf1c89af54138765970
Rui <rui.albuquerque.dorey@gmail.com>
2018-02-25 17:15:57 +0000

    name on gameobject and mesh derived by classname

--- puzzle-bobble/src/logic/GameObject.js
+++ puzzle-bobble/src/logic/GameObject.js
@@ -1,113 +1,122 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import * as THREE from 'three';
 
 export class GameObject extends Component {
 
-    mesh = new THREE.Object3D();
-    components = {};
-    childGameObjects = [];
-    id = '';
-    i = 0;
+  mesh = new THREE.Object3D();
+  components = {};
+  childGameObjects = [];
+  id = '';
+  i = 0;
 
-    componentDidMount() {
-        this.id = this.props.id;
-        this.buildComponents();
-        this.initComponents();
-        this.start();
+  componentDidMount()
+    {
+      this.id = this.props.id || this.constructor.name;
+      this.mesh.name = this.id + 'Mesh';
+      this.buildComponents();
+      this.initComponents();
+      this.start();
     }
 
-    start = ()=> {
+  start = () => {
+  };
+
+  _update = () => {
+    this.updateComponents();
+    this.updateChildren();
+    this.update();
+  };
+
+  updateChildren()
+    {
+      this.childGameObjects.forEach( ( gameobject ) => {
+        gameobject._update();
+      } );
     }
 
-    _update = () => {
-        this.updateComponents();
-        this.updateChildren();
-        this.update();
-    };
-
-    updateChildren() {
-        this.childGameObjects.forEach( ( gameobject ) => {
-            gameobject._update();
-        } );
-    }
-
-    updateComponents() {
-        for ( const componentID in this.components ) {
-            this.components[ componentID ]._update( this.props.components[ componentID ] );
+  updateComponents()
+    {
+      for ( const componentID in this.components )
+        {
+          this.components[componentID]._update(
+              this.props.components[componentID] );
         }
     }
 
-    update = () => {
-    };
+  update = () => {
+  };
 
-    buildComponents() {
+  buildComponents()
+    {
     }
 
-    initComponents() {
+  initComponents()
+    {
     }
 
-    buildChildren = ( children ) => {
-        const _children = [];
-        if ( !children ) {
-            console.log( 'GameObject ${this.id} has no children to build.' );
-        }
+  buildChildren = ( children ) => {
+    const _children = [];
+    if ( !children )
+      {
+        console.log( 'GameObject ${this.id} has no children to build.' );
+      }
 
-        this.childGameObjects = this.buildChildGameObjects( children.gameobjects );
-    };
+    this.childGameObjects = this.buildChildGameObjects( children.gameobjects );
+  };
 
-    buildChildGameObjects = ( children ) => {
+  buildChildGameObjects = ( children ) => {
 
-    };
+  };
 
-
-    render() {
-        this.i++;
-        console.log( 'render i:${this.i}' );
-        return null;
+  render()
+    {
+      this.i++;
+      console.log( 'render i:${this.i}' );
+      return null;
     }
 
-    /*+
-     {
-     let result: any[] = []
-     let DynamicComponentType: typeof Component3D | undefined;
+  /*+
+   {
+   let result: any[] = []
+   let DynamicComponentType: typeof Component3D | undefined;
 
-     if ( this.props.elementData.components )
-     {
-     Object.keys( this.props.elementData.components ).forEach( componentName => {
-     DynamicComponentType = this.getTypeOfComponent( componentName );
+   if ( this.props.elementData.components )
+   {
+   Object.keys( this.props.elementData.components ).forEach( componentName => {
+   DynamicComponentType = this.getTypeOfComponent( componentName );
 
-     if ( !DynamicComponentType )
-     {
-     return;
-     }
+   if ( !DynamicComponentType )
+   {
+   return;
+   }
 
-     const _id = this.id + '_component_' + componentName;
-     const _parameters = this.props.elementData.components[ componentName ];
-     result.push( <DynamicComponentType getLogger={this.props.getLogger}
-     isSelected={this.state.selected} key={_id} id={_id}
-     entity={this} parameters={_parameters}
-     geometryWasUpdated={this.state.geometryWasUpdated}
-     updateComponentState={this.props.updateComponentState}
-     batchUpdateEntityStore={this.props.batchUpdateEntityStore}
-     setRendererState={this.props.setRendererState}
-     getRendererState={this.props.getRendererState}
-     markGeometryAsNeedingUpdate={
-     this._markGeometryAsNeedingUpdate}
-     registerComponent={this.registerComponent}
-     deregisterComponent={this.deregisterComponent}
-     getEntityPivotMesh={this.getPivotMesh}
-     entityTransformWasUpdated={
-     this.updatedTransform}
-     attachMeshComponent={this.attachMeshComponent}/> );
-     } );
+   const _id = this.id + '_component_' + componentName;
+   const _parameters = this.props.elementData.components[ componentName ];
+   result.push( <DynamicComponentType getLogger={this.props.getLogger}
+   isSelected={this.state.selected} key={_id} id={_id}
+   entity={this} parameters={_parameters}
+   geometryWasUpdated={this.state.geometryWasUpdated}
+   updateComponentState={this.props.updateComponentState}
+   batchUpdateEntityStore={this.props.batchUpdateEntityStore}
+   setRendererState={this.props.setRendererState}
+   getRendererState={this.props.getRendererState}
+   markGeometryAsNeedingUpdate={
+   this._markGeometryAsNeedingUpdate}
+   registerComponent={this.registerComponent}
+   deregisterComponent={this.deregisterComponent}
+   getEntityPivotMesh={this.getPivotMesh}
+   entityTransformWasUpdated={
+   this.updatedTransform}
+   attachMeshComponent={this.attachMeshComponent}/> );
+   } );
 
-     }
-     let childReactComponents: any[] = [];
-     childReactComponents = this.buildChildrenReactComponents( this.props.elementData.id );
-     result.push( ...childReactComponents );
-     return <div key={this.id}>{result}</div>;
-     }
-     */
+   }
+   let childReactComponents: any[] = [];
+   childReactComponents = this.buildChildrenReactComponents( this.props.elementData.id );
+   result.push( ...childReactComponents );
+   return <div key={this.id}>{result}</div>;
+   }
+   */
 
 }
 
