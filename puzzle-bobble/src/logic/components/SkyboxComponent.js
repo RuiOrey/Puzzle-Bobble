b11a24c019e001402ae316a3b4aaf7e4257a40a4
Rui <rui.albuquerque.dorey@gmail.com>
2018-02-26 18:32:30 +0000

    changed skybox

--- puzzle-bobble/src/logic/components/SkyboxComponent.js
+++ puzzle-bobble/src/logic/components/SkyboxComponent.js
@@ -2,56 +2,62 @@ import   TWEEN  from '@tweenjs/tween.js';
 
 import * as THREE from 'three';
 
-export const isSkybox = ( object, parameters ) => {
-    const _object = object;
+export const isSkybox = ( object, parameters ) =>
+    {
+        const _object = object;
 
-    let _parameters = {
-        skyboxType: "sphere",
-        texture: "/assets/img/sky.png",
-        radius: 100
+        let _parameters = {
+            skyboxType: "sphere",
+            // texture: "/assets/img/sky.png",
+            texture: "/assets/img/PIA15482.jpg",
+            radius: 100
 
-    }
-
-    const loader = new THREE.TextureLoader();
-    let _texture;
-    loader.load(
-        _parameters.texture,
-
-        function ( texture ) {
-            _texture = texture;
-            _texture.wrapS = THREE.MirroredRepeatWrapping;
-            texture.repeat.set( 2, 1 );
-            if ( _skybox ) {
-                _skybox.material.envMap = texture;
-                _skybox.material.map = texture;
-                _skybox.material.needsUpdate = true;
-            }
-        },
-        undefined,
-        function ( err ) {
-            console.error( 'An error loading texture happened.' );
         }
-    );
 
-    const _material = new THREE.MeshStandardMaterial( {
-        side: THREE.DoubleSide,
-        transparent: true,
-        opacity: 1.0,
-        roughness: 0.2,
-        metalness: 0.5
+        const loader = new THREE.TextureLoader();
+        let _texture;
+        loader.load(
+            _parameters.texture,
 
-    } );
+            function ( texture )
+            {
+                _texture = texture;
+                _texture.wrapS = THREE.MirroredRepeatWrapping;
+                texture.repeat.set( 1, 1 );
+                if ( _skybox )
+                    {
+                        _skybox.material.envMap = texture;
+                        _skybox.material.map = texture;
+                        _skybox.material.needsUpdate = true;
+                    }
+            },
+            undefined,
+            function ( err )
+            {
+                console.error( 'An error loading texture happened.' );
+            }
+        );
 
-    const _skyboxGeo = new THREE.SphereGeometry( _parameters.radius );
+        const _material = new THREE.MeshStandardMaterial( {
+            side: THREE.DoubleSide,
+            transparent: true,
+            opacity: 1.0,
+            roughness: 0.2,
+            metalness: 0.5,
 
-    const _skybox = new THREE.Mesh( _skyboxGeo, _material );
+            refractionRatio: 0.1
 
+        } );
 
-    let state = {
-        mesh: _skybox
-    };
+        const _skyboxGeo = new THREE.SphereGeometry( _parameters.radius );
 
-    return {
-        skybox: state
-    };
-}
+        const _skybox = new THREE.Mesh( _skyboxGeo, _material );
+
+        let state = {
+            mesh: _skybox
+        };
+
+        return {
+            skybox: state
+        };
+    }
