effb73a2358caff2ebd272b07e39f16796628e26
Rui <rui.albuquerque.dorey@gmail.com>
2018-02-26 03:05:18 +0000

    debug changecamera on DynamicCameraPrefab

--- puzzle-bobble/src/logic/Input.js
+++ puzzle-bobble/src/logic/Input.js
@@ -1,37 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 export class Input extends Component {
 
-    events: {
-        "ArrowLeft":"moveleft",
-        "ArrowUp": "moveright"
-    };
+  events: {
+    'ArrowLeft': 'moveleft',
+    'ArrowUp': 'moveright'
+  };
 
-    componentDidMount() {
+  componentDidMount()
+    {
 
-        const moveleft = new Event('moveleft');
-        const moveright = new Event('moveright');
-        const shoot = new Event('shoot');
+      ;
 
-        document.addEventListener('keydown', function(event) {
-            console.log("Pressed: ", event.keyCode);
+      document.addEventListener( 'keydown', function( event ) {
+        console.log( 'Pressed: ', event.keyCode );
 
-            if (event.key === "ArrowLeft") {
-                document.dispatchEvent(moveleft, false);
-            }
+        if ( event.key === 'ArrowLeft' )
+          {
+            document.dispatchEvent( new Event( 'moveleft' )
+                ,
+                false,
+            )
+            ;
+          }
 
-            if (event.key === "ArrowRight") {
-                document.dispatchEvent(moveright, false);
-            }
+        if ( event.key === 'ArrowRight' )
+          {
+            document.dispatchEvent( new Event( 'moveright' )
+                ,
+                false,
+            )
+            ;
+          }
 
-            if (event.keyCode === 32) {
-                document.dispatchEvent(shoot, false);
-            }
-        });
+        if ( event.keyCode === 32 )
+          {
+            document.dispatchEvent( new Event( 'shoot' ), false );
+          }
+
+        if ( event.keyCode === 67 )
+          {
+            document.dispatchEvent( new Event( 'camera_change' ), false );
+          }
+      } );
     }
 
-    render() {
-        return null
+  render()
+    {
+      return null;
     }
 
 }
--- puzzle-bobble/src/logic/prefabs/DynamicCameraPrefab.js
+++ puzzle-bobble/src/logic/prefabs/DynamicCameraPrefab.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Camera } from '../../render/Camera';
-import { isTraveler } from "../components/TravelerComponent";
+import {Camera} from '../../render/Camera';
+import {isTraveler} from '../components/TravelerComponent';
+
+import * as THREE from 'three'; // TODO:Remove debug
 
 export class DynamicCameraPrefab extends Camera {
 
-    componentDidMount() {
-        super.componentDidMount();
-        Object.assign( this, isTraveler( this.mesh ) );
+  componentDidMount()
+    {
+      super.componentDidMount();
+      Object.assign( this, isTraveler( this.mesh ) );
+      document.addEventListener( 'camera_change', () => {
+        this.travel.travelTo(
+            new THREE.Vector3( 60 - 120 * Math.random(), 60 - 120 *
+                Math.random(), 60 - 120 *
+                Math.random() ), 1000, {target: new THREE.Vector3( 0, 0, 0 )} );
+      } );
     }
 
-    render() {
-        return <div>DynamicCameraPrefab</div>;
+  render()
+    {
+      return <div>DynamicCameraPrefab</div>;
     }
 }
