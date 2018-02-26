ceea1fb8b1dc740a13c39074cebb7d249dd1fa8b
Rui <rui.albuquerque.dorey@gmail.com>
2018-02-26 02:07:42 +0000

    init ball component

---
+++ puzzle-bobble/src/logic/components/BallComponent.js
@@ -0,0 +1,6 @@
+import TWEEN from '@tweenjs/tween.js';
+
+import * as THREE from 'three';
+
+export const isBall = ( object, parameters ) => {
+};
--- puzzle-bobble/src/logic/prefabs/BallPrefab.js
+++ puzzle-bobble/src/logic/prefabs/BallPrefab.js
@@ -1,10 +1,22 @@
 import React, {Component} from 'react';
+import {GameObject} from '../GameObject';
 
-class $Title$ extends Component {
+import {isTraveler} from '../components/TravelerComponent';
+import {isBall} from '../components/BallComponent';
+
+class BallPrefab extends GameObject {
+
+  buildComponents = () => {
+    this.parameters = this.props.parameters ? this.props.parameters : {};
+    this.parameters = Object.assign( {ball: {}}, this.parameters );
+    Object.assign( this.components, isBall( this, this.parameters.ball ) );
+    Object.assign( this, isTraveler( this.mesh ) );
+  };
+
   render()
     {
       return null;
     }
 }
 
-export default $Title$;
+export default BallPrefab;
