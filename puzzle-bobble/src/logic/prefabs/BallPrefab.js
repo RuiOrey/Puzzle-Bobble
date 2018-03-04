f7efc80b9218f6f037d7888d0c1fc1852ef40dae
Rui <rui.albuquerque.dorey@gmail.com>
2018-03-04 13:51:50 +0000

    physics component init

--- puzzle-bobble/src/logic/prefabs/BallPrefab.js
+++ puzzle-bobble/src/logic/prefabs/BallPrefab.js
@@ -1,21 +1,22 @@
-import React, {Component} from 'react';
-import {GameObject} from '../GameObject';
+import React, { Component } from 'react';
+import { GameObject } from '../GameObject';
 
-import {isTraveler} from '../components/TravelerComponent';
-import {isBall} from '../components/BallComponent';
+import { isTraveler } from '../components/TravelerComponent';
+import { isBall } from '../components/BallComponent';
+import { hasPhysics } from '../components/PhysicsComponent';
 
 class BallPrefab extends GameObject {
 
-  buildComponents = () => {
-    this.parameters = this.props.parameters ? this.props.parameters : {};
-    this.parameters = Object.assign( {ball: {}}, this.parameters );
-    Object.assign( this.components, isBall( this, this.parameters.ball ) );
-    Object.assign( this, isTraveler( this.mesh ) );
-  };
+    buildComponents = () => {
+        this.parameters = this.props.parameters ? this.props.parameters : {};
+        this.parameters = Object.assign( { ball: {} }, this.parameters );
+        Object.assign( this.components, isBall( this, this.parameters.ball ) );
+        Object.assign( this, isTraveler( this.mesh ) );
+        Object.assign( this, hasPhysics( this, "sphere" ) );
+    };
 
-  render()
-    {
-      return null;
+    render() {
+        return null;
     }
 }
 
